970c76b578e4b184bbd94278b8f9b15fabe1a0d9
adame222212 <70124453+adame222212@users.noreply.github.com>
2023-09-23 16:29:00 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+alert("Virus detected!, Terminate immediately");
+alert("Just kidding");
+alert("keep pressing the button 'OK'🥺 ");
+alert("You're doing Great, Keep Fighting");
+alert(
+  "Sometimes, all you can do is take one small step forward. That's still progress."
+);
+alert(
+  "Burnout can lead to giving up. Make sure to take breaks and care for your physical and mental well-being.☺️"
+);
+alert(
+  "Reflect on why you started this journey in the first place. Connecting with your initial motivation can reignite your passion. 😘"
+);
+alert(
+  "Remember that giving up is often the easy way out, and it rarely leads to the fulfillment of your dreams and goals. Stay committed, and over time, your persistence and effort will likely lead to success.🤗"
+);
+alert("Here a Hug for you");
